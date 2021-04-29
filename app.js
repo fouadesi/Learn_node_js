b964b0d950c0af0db070976a93915f5e5c3646d1
fouadesi <53347470+fouadesi@users.noreply.github.com>
2021-04-29 12:09:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,27 +7,31 @@ const app = express();
 const nodemailer = require("nodemailer");
 const mysql = require('mysql');
 
-const db = mysql.createConnection({
-    host : "34.72.148.89", 
-    user : 'root', 
-    password : '12345678',
-    database : 'hygeiadb', 
-})
-console.log("wayli wayli");
-db.connect((err,connection)=> {
-    if(err) {
-        console.log(err)
-    }else {
-        console.log("connection created succefullyyyyyyyyy")
-    }
-});
+
 const bodyParser = require('body-parser');
 
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+app.use('/',(req,res,next)=> {
+    const db = mysql.createConnection({
+        host : "34.72.148.89", 
+        user : 'root', 
+        password : '12345678',
+        database : 'hygeiadb', 
+    })
+    console.log("wayli wayli");
+    db.connect((err,connection)=> {
+        if(err) {
+            res.send(err)
+            console.log(err)
+        }else {
+            res.send("conncted");
+            console.log("connection created succefullyyyyyyyyy")
+        }
+    });
+})
 app.set("app engine","ejs"); 
 app.set('views','views/ejs');    
 app.use('/home', (req,res,next)=> {
